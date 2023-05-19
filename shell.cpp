9a6fd0dcd3190158c37478e6264b6756ede666ea
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-05-19 10:44:25 -0700

    Implemented a simple shell in c++

--- shell.cpp
+++ shell.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+#include <unistd.h>
 
-int main(int argc, char** argv) {
-
-    
-
-    return 0;
+std::vector<std::string> tokenize(std::string line) {
+    std::vector<std::string> tokens;
+    std::stringstream stream(line);
+    std::string token; 
+    while (stream >> token) {
+        tokens.push_back(token);
+    }
+    return tokens;
 }
 
+int main(int argc, char** argv) {
+    std::string line;
+    int status;
+    while (true) {
+        
+        // Read line from standard input.
+        std::cout << "$ ";
+        std::getline(std::cin, line);
+
+        // Tokenize
+        auto args = tokenize(line);
+
+        // Check if the command is a builtin
+        if (args[0] == "cd" && args.size() > 1) {
+            chdir(args[1].c_str());
+        
+        // External program
+        } else  { 
+            std::flush(std::cout);
+            status = std::system(line.c_str());
+        }
+    }
+    return 0;
+}
